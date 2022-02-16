36c9e276ed353f47cbf5a126dad5f1ad1a9c5f6f
niktopelo <niktopelo@gmail.com>
2022-02-16 23:44:23 +0100

    dl files works

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var router = express.Router();
 var bodyParser = require("body-parser");
 
 const fs = require('fs');
-const { promisify } = require('util')
+const {promisify} = require('util')
 // TO USE IT ASYNC maybe writeFileSync() is better
 const writeFileAsync = promisify(fs.writeFile)
 const readFileAsync = promisify(fs.readFile)
@@ -31,125 +31,166 @@ app.listen(PORT_EXT, HOST_NAME, function () {
 router.route('/scenarios')
     .get(async function (req, res) {
         const scenariosFolders = 'data/scenarios';
-       const scenariosData = fs.readdirSync(scenariosFolders).map(  folder => {
-           const scenario =  fs.readdirSync(scenariosFolders+'/'+folder).reduce( (acumulator,subContent) => {
-               if(subContent.includes('.json')){
-                   const data = fs.readFileSync(scenariosFolders+'/'+folder+'/'+subContent)
-                   const dataParsed = JSON.parse(data)
-                   dataParsed.title = folder
-                   const reponsesData =  dataParsed.properties.questions.default
-                   dataParsed.properties.questions.default = reponsesData.map(reponseData => {
-                       if (reponseData.responses.Images){
-                           reponseData.responses.Images = reponseData.responses.Images.map(image => {
-                               const image_in_base64  = fs.readFileSync(image.path, 'base64');
-                               // we must get the extension of file
-                               const fileExt = image.path.split('.')[1]
-                               image.file = 'data:image/'+fileExt+';base64,'+image_in_base64;
-                               delete image.path
-                               return image;
-                           })
-                       }
-                       if (reponseData.responses.Videos){
-                           reponseData.responses.Videos = reponseData.responses.Videos.map(video => {
-                               const video_in_base64  = fs.readFileSync(video.path, 'base64');
-                               // we must get the extension of file
-                               const fileExt = video.path.split('.')[1]
-                               video.file = 'data:image/'+fileExt+';base64,'+video_in_base64;
-                               delete video.path
-                               return video;
-                           })
-                       }
-                       return reponseData;
-                   })
-                   // maybe get the files and change them in base 64 ?
-                   acumulator = {...acumulator,...dataParsed };
-               }
-               return acumulator
-           },{});
-           return scenario
-       });
+        const scenariosData = fs.readdirSync(scenariosFolders).map(folder => {
+            const scenario = fs.readdirSync(scenariosFolders + '/' + folder).reduce((acumulator, subContent) => {
+                if (subContent.includes('.json')) {
+                    const data = fs.readFileSync(scenariosFolders + '/' + folder + '/' + subContent)
+                    const dataParsed = JSON.parse(data)
+                    dataParsed.title = folder
+                    const reponsesData = dataParsed.properties.questions.default
+                    dataParsed.properties.questions.default = reponsesData.map(reponseData => {
+                        if (reponseData.responses.Images) {
+                            reponseData.responses.Images = reponseData.responses.Images.map(image => {
+                                const image_in_base64 = fs.readFileSync(image.path, 'base64');
+                                // we must get the extension of file
+                                const fileExt = image.path.split('.')[1]
+                                image.file = 'data:image/' + fileExt + ';base64,' + image_in_base64;
+                                delete image.path
+                                return image;
+                            })
+                        }
+                        if (reponseData.responses.Videos) {
+                            reponseData.responses.Videos = reponseData.responses.Videos.map(video => {
+                                const video_in_base64 = fs.readFileSync(video.path, 'base64');
+                                // we must get the extension of file
+                                const fileExt = video.path.split('.')[1]
+                                video.file = 'data:image/' + fileExt + ';base64,' + video_in_base64;
+                                delete video.path
+                                return video;
+                            })
+                        }
+                        return reponseData;
+                    })
+                    // maybe get the files and change them in base 64 ?
+                    acumulator = {...acumulator, ...dataParsed};
+                }
+                return acumulator
+            }, {});
+            return scenario
+        });
         res.json(scenariosData);
 
     })
-    .post( async (req, res) => {
+    .post(async (req, res) => {
         let schema = req.body.data;
         // first we need to create a direcory
-        const mainDirectoryName =  'data/scenarios/' + req.body.fileName
-        if (!fs.existsSync(mainDirectoryName)){
-            fs.mkdirSync(mainDirectoryName,{ recursive: true });
-            fs.mkdirSync(mainDirectoryName+'/images');
-            fs.mkdirSync(mainDirectoryName+'/videos');
-        }else {
-             //we can delete here all videos et photos files because if the file exist we do modification
+        const mainDirectoryName = 'data/scenarios/' + req.body.fileName
+        if (!fs.existsSync(mainDirectoryName)) {
+            fs.mkdirSync(mainDirectoryName, {recursive: true});
+            fs.mkdirSync(mainDirectoryName + '/images');
+            fs.mkdirSync(mainDirectoryName + '/videos');
+        } else {
+            //we can delete here all videos et photos files because if the file exist we do modification
         }
-        if (schema.properties.questions){
+        if (schema.properties.questions) {
             let i = 0;
             for await (question of schema.properties.questions.default) {
                 // we need to separate the files
                 // after we save the files and we get te path of each file
                 // add the path to the questionnary
-                if (question.responses){
+                if (question.responses) {
                     let imgIndex = 1;
                     question.responses.Images = await Promise.all(question.responses.Images.map(async (image) => {
                         let base64Array = image.file.split(';base64,');
                         const type = base64Array[0].split('data:image/')[1];
-                        const imageName = 'img'+imgIndex
-                        const path = mainDirectoryName + '/images/' +imageName  + '.' + type;
+                        const imageName = 'img' + imgIndex
+                        const path = mainDirectoryName + '/images/' + imageName + '.' + type;
                         await writeFileAsync(path, base64Array[1], {encoding: 'base64'});
                         delete image.file;
-                        imgIndex ++;
+                        imgIndex++;
                         return {path: path, ...image}
                     }));
                     let videoIndex = 1;
                     question.responses.Videos = await Promise.all(question.responses.Videos.map(async (video) => {
-                            let base64Array = video.file.split(';base64,');
-                            const type = base64Array[0].split('data:video/')[1];
-                            const videoName = 'video' + videoIndex
-                            const path = mainDirectoryName + '/videos/' + videoName + '.' + type;
-                            await writeFileAsync(path, base64Array[1], {encoding: 'base64'});
-                            delete video.file;
-                            videoIndex ++ ;
-                            return {path: path, ...video}
-                        }));
-                        schema.properties.questions.default[i] = question;
-                        i++;
+                        let base64Array = video.file.split(';base64,');
+                        const type = base64Array[0].split('data:video/')[1];
+                        const videoName = 'video' + videoIndex
+                        const path = mainDirectoryName + '/videos/' + videoName + '.' + type;
+                        await writeFileAsync(path, base64Array[1], {encoding: 'base64'});
+                        delete video.file;
+                        videoIndex++;
+                        return {path: path, ...video}
+                    }));
+                    schema.properties.questions.default[i] = question;
+                    i++;
                 }
             }
         }
         const jsonContent = JSON.stringify(schema);
-        const fullPath = mainDirectoryName+'/'+req.body.fileName+'.json';
-        await writeFileAsync( fullPath, jsonContent, 'utf8', function (err) {
+        const fullPath = mainDirectoryName + '/' + req.body.fileName + '.json';
+        await writeFileAsync(fullPath, jsonContent, 'utf8', function (err) {
             if (err) {
                 return console.log(err);
             }
         });
 
-        res.json({data : req.body,
-            methode: req.method});
+        res.json({
+            data: req.body,
+            methode: req.method
+        });
     })
     .delete(function (req, res) {
         const scenariosFolders = 'data/scenarios/';
         let scenarioName = req.body.title;
         let targetDir = scenariosFolders + scenarioName;
-        if (fs.existsSync(targetDir)){
-            fs.rmSync(targetDir, { recursive: true, force: true });
-            res.json('Scenario deleted');
-        }else {
-            res.json('Scenario not found');
+        if (fs.existsSync(targetDir)) {
+            fs.rmSync(targetDir, {recursive: true, force: true});
+            return res.json('Scenario deleted');
+        } else {
+            return res.json('Scenario not found');
         }
     })
     .put(function (req, res) {
     });
 
 router.route('/scenariosExport')
-    .post( async (req, res) => {
+    .post(async (req, res) => {
         let schema = req.body.data;
-        const mainDirectoryName =  'data/scenarios/' + req.body.fileName
-        if (!fs.existsSync(mainDirectoryName)){
+        const mainDirectoryName = 'data/scenarios/' + req.body.fileName
+        if (!fs.existsSync(mainDirectoryName)) {
             res.json('scenario don"t exist');
         }
 
 
-        res.json({data : req.body,
-            methode: req.method});
+        return res.json({
+            data: schema,
+            methode: req.method
+        });
+    })
+
+
+router.route('/scenarioFilesPath')
+    .post((req, res) => {
+        const main_directory_name = 'data/scenarios/' + req.body.fileName;
+        if (!fs.existsSync(main_directory_name)) {
+            return res.json('scenario don"t exist');
+        }
+        if (!fs.existsSync(main_directory_name + '/' + req.body.fileName + '.pdf')) {
+            return res.json('scenario have no exports files');
+        }
+        let files_paths = {
+            'pdf': {'name': 'export', 'path': main_directory_name + '/' + req.body.fileName + '.pdf'},
+            'img': [], 'videos': []
+        }
+        if (fs.existsSync(main_directory_name + '/images')) {
+            files_paths.img = fs.readdirSync(main_directory_name + '/images').map(imageName => {
+                return {'name': imageName, 'path': main_directory_name + '/images/' + imageName};
+            })
+            files_paths.videos = fs.readdirSync(main_directory_name + '/videos').map(videoNAme => {
+                return {'name': videoNAme, 'path': main_directory_name + '/videos/' + videoNAme};
+            })
+        }
+        return res.json(files_paths);
+    })
+
+router.route('/dlFile')
+    .get((req, res) => {
+        let path = req.query.path;
+        if (fs.existsSync(path)) {
+            return res.download(path)
+        } else {
+            return res.json('scenario don"t exist');
+        }
+
+
     })
