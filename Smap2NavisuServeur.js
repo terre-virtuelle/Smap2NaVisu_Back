dc3fa69ea0f83ad37da384c20de5f0430edfa3ef
liboub <a.libouban@laposte.net>
2022-06-03 11:39:15 +0200

    add try catch on api to prevent of bugs

--- Smap2NavisuServeur.js
+++ Smap2NavisuServeur.js
@@ -26,40 +26,55 @@ app.listen(PORT_EXT, HOST_NAME, function () {
 
 router.route('/scenarios')
     .get(async (req, res) => {
-        // need change here
-        const scenariosData = fs.readdirSync(scenariosFolders).map(folder => {
-            // maybe a a filter is better
-            const scenarioFiles = fs.readdirSync(scenariosFolders + '/' + folder).reduce((acumulator, subContent) => {
-                if (subContent.includes('.json')) {
-                    const data = fs.readFileSync(scenariosFolders + '/' + folder + '/' + subContent)
-                    const scenarioObj = new ScenarioModel(JSON.parse(data),scenariosFolders);
-                    scenarioObj.formatForRes();
-                    scenarioObj.fileName = folder;
-                    acumulator = {...acumulator, ...scenarioObj};
-                }
-                return acumulator;
-            }, {});
-            return scenarioFiles;
-        });
-        res.json(scenariosData);
+        try {
+            // need change here
+            const scenariosData = fs.readdirSync(scenariosFolders).map(folder => {
+                // maybe a a filter is better
+                const scenarioFiles = fs.readdirSync(scenariosFolders + '/' + folder).reduce((acumulator, subContent) => {
+                    if (subContent.includes('.json')) {
+                        const data = fs.readFileSync(scenariosFolders + '/' + folder + '/' + subContent)
+                        const scenarioObj = new ScenarioModel(JSON.parse(data), scenariosFolders);
+                        scenarioObj.formatForRes();
+                        scenarioObj.fileName = folder;
+                        acumulator = {...acumulator, ...scenarioObj};
+                    }
+                    return acumulator;
+                }, {});
+                return scenarioFiles;
+            });
+            res.json(scenariosData);
+        } catch (er) {
+            console.log(er);
+            res.json('error', er);
+        }
 
     })
     .post(async (req, res) => {
-        const scenario = new ScenarioModel(req.body.data,scenariosFolders)
-       await  scenario.save(req.body.fileName);
-        return res.json({
-            data: req.body,
-            methode: req.method
-        });
+        try {
+            const scenario = new ScenarioModel(req.body.data, scenariosFolders)
+            await scenario.save(req.body.fileName);
+            return res.json({
+                data: req.body,
+                methode: req.method
+            });
+        } catch (er) {
+            console.log(er);
+            res.json('error', er);
+        }
     })
     .delete(function (req, res) {
-        let scenarioName = req.body.title;
-        let targetDir = scenariosFolders + scenarioName;
-        if (fs.existsSync(targetDir)) {
-            fs.rmSync(targetDir, {recursive: true, force: true});
-            return res.json('Scenario deleted');
-        } else {
-            return res.json('Scenario not found');
+        try {
+            let scenarioName = req.body.title;
+            let targetDir = scenariosFolders + scenarioName;
+            if (fs.existsSync(targetDir)) {
+                fs.rmSync(targetDir, {recursive: true, force: true});
+                return res.json('Scenario deleted');
+            } else {
+                return res.json('Scenario not found');
+            }
+        } catch (er) {
+            console.log(er);
+            res.json('error', er);
         }
     })
     .put(function (req, res) {
@@ -67,34 +82,44 @@ router.route('/scenarios')
 
 router.route('/scenarioFilesPath')
     .post((req, res) => {
-        const main_directory_name = scenariosFolders + '/' + req.body.fileName;
-        if (!fs.existsSync(main_directory_name)) {
-            return res.json('scenario don"t exist');
+        try {
+            const main_directory_name = scenariosFolders + '/' + req.body.fileName;
+            if (!fs.existsSync(main_directory_name)) {
+                return res.json('scenario don"t exist');
+            }
+            if (!fs.existsSync(main_directory_name + '/pdf/' + req.body.fileName + '.pdf')) {
+                return res.json('scenario have no exports files');
+            }
+            let files_paths = {
+                'pdf': {'name': 'export', 'path': main_directory_name + '/pdf/' + req.body.fileName + '.pdf'},
+                'img': [], 'videos': []
+            }
+            if (fs.existsSync(main_directory_name + '/images')) {
+                files_paths.img = fs.readdirSync(main_directory_name + '/images').map(imageName => {
+                    return {'name': imageName, 'path': main_directory_name + '/images/' + imageName};
+                })
+                files_paths.videos = fs.readdirSync(main_directory_name + '/videos').map(videoNAme => {
+                    return {'name': videoNAme, 'path': main_directory_name + '/videos/' + videoNAme};
+                })
+            }
+            return res.json(files_paths);
+        } catch (er) {
+            console.log(er);
+            res.json('error', er);
         }
-        if (!fs.existsSync(main_directory_name + '/pdf/' + req.body.fileName + '.pdf')) {
-            return res.json('scenario have no exports files');
-        }
-        let files_paths = {
-            'pdf': {'name': 'export', 'path': main_directory_name + '/pdf/' + req.body.fileName + '.pdf'},
-            'img': [], 'videos': []
-        }
-        if (fs.existsSync(main_directory_name + '/images')) {
-            files_paths.img = fs.readdirSync(main_directory_name + '/images').map(imageName => {
-                return {'name': imageName, 'path': main_directory_name + '/images/' + imageName};
-            })
-            files_paths.videos = fs.readdirSync(main_directory_name + '/videos').map(videoNAme => {
-                return {'name': videoNAme, 'path': main_directory_name + '/videos/' + videoNAme};
-            })
-        }
-        return res.json(files_paths);
     })
 
 router.route('/dlFile')
     .get((req, res) => {
-        let path = req.query.path;
-        if (fs.existsSync(path)) {
-            return res.download(path)
-        } else {
-            return res.json('scenario don"t exist');
+        try {
+            let path = req.query.path;
+            if (fs.existsSync(path)) {
+                return res.download(path)
+            } else {
+                return res.json('scenario don"t exist');
+            }
+        } catch (er) {
+            console.log(er);
+            res.json('error', er);
         }
     })
